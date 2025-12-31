d90593c0ab7c3247cf12788699ba441e55f03011
Moshe <moshe@example.com>
2025-12-31 11:01:44 +0200

    finish manager and fix api

--- app/main.py
+++ app/main.py
@@ -1,52 +1,54 @@
-# from fastapi import FastAPI, HTTPException, status
-# from schemas_api import Contact
-# import uvicorn
-# from db_manager import MongoDBManagerContacs
-# import os
+from fastapi import FastAPI, HTTPException, status
+from schemas_api import Contact
+import uvicorn
+from db_manager import MongoDBManagerContacs
+from bson import ObjectId
 
 
 
-# app = FastAPI()
-# db = MongoDBManagerContacs
-
-# @app.get('/contacts')
-# def get_all_contacts():
-#     try:
-#         contacts =  db.database.get_all()
-#         if not contacts:
-#             raise HTTPException(
-#                 status_code=status.HTTP_404_NOT_FOUND,
-#                 detail=f"There is no contacts")
-#         return contacts
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=f"Database connection error: {str(e)}")
 
 
-# @app.post('/contacts')
-# def create_contact(contact:Contact):
-#     try:
-#         contact_py = contact.model_dump()
-#         new_id = db.database.create_contact(TABLE_NAME, contact_py)
-#         return {'message':"Contact created successfully",
-#                 'id':new_id}
-#     except Exception as e:
-#         raise HTTPException(status_code=400, detail=f"Could not create contact: {str(e)}")
+app = FastAPI()
+db = MongoDBManagerContacs()
 
-# @app.put('/contacts/{id_contact}')
-# def update_contact(id_contact:int, new_info:Contact):
-#     contact = new_info.model_dump()
-#     if db.database.update_record(TABLE_NAME, id_contact, contact):
-#         return {"status": "update"}
-#     else:
-#         return {"status":"error"}    
+@app.get('/contacts')
+def get_all_contacts():
+    try:
+        contacts =  db.get_all_contacts()
+        if not contacts:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"There is no contacts")
+        return contacts
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Database connection error: {str(e)}")
 
 
+@app.post('/contacts')
+def create_contact(contact:Contact):
+    try:
+        contact_py = contact.model_dump()
+        new_id = db.insert_contact(contact_py)
+        return {'message':"Contact created successfully",
+                'id':str(new_id)}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Could not create contact: {str(e)}")
 
-# @app.delete('/contacts/{id_contact}')
-# def delete_contact(id_contact:int):
+@app.put('/contacts/{id_contact}')
+def update_contact(id_contact:str, new_info:Contact):
+    contact = new_info.model_dump()
+    if db.update_contact(ObjectId(id_contact), contact):
+        return {"status": "update"}
+    else:
+        return {"status":"error"}    
+
+
+@app.delete('/contacts/{id_contact}')
+def delete_contact(id_contact:str):
+    if db.delete_contact(ObjectId(id_contact)):
+        return {"status": "deleted"}
+    else:
+        return {"status":"error"}    
     
-#     if db.database.delete_record(TABLE_NAME, id_contact):
-#         return {"status": "deleted"}
-#     else:
-#         return {"status":"error"}    
-    
+if __name__ == '__main__':
+    uvicorn.run(app,host='127.0.0.1',port=8000)
